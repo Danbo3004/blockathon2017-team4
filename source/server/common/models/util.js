4c7235a9c0ba98bffe9797fe14afae88e2f47e42
CS Huynh Hoang <huynhphan@clearsystems.asia>
2017-11-24 23:09:10 +0700

    create deploy contract api, return transaction hash

--- source/server/common/models/util.js
+++ source/server/common/models/util.js
@@ -44,4 +44,58 @@ module.exports = function(Util) {
     }
     globals['eth-node'].eth.getBalance(address, cb);
   }
+
+  Util.remoteMethod('deployContract', {
+    accepts: [
+      {arg: 'contractName', type: 'string', required: true},
+      {arg: 'args', type: 'array'}
+    ],
+    http: {
+      verb: 'get'
+    },
+    returns: {
+      arg: 'transactionHash', type: 'string'
+    }
+  })
+  Util.deployContract = function(contractName, args, cb) {
+    if (!globals['smart-contracts'][contractName]) {
+      let err = new Error('contract name not found');
+      err.statusCode = 404;
+      cb(err);
+      return;
+    }
+    const bytecode = globals['smart-contracts'][contractName].bytecode;
+    const abi = globals['smart-contracts'][contractName].abi;
+    const contract = new globals['eth-node'].eth.Contract(abi);
+    const deployingContract = contract.deploy({
+      data: bytecode,
+      arguments: args
+    });
+    async.auto({
+      getGasPrice: function(callback) {
+        globals['eth-node'].eth.getGasPrice(callback);
+      },
+      getEstimateGas: function(callback) {
+        deployingContract.estimateGas({}, callback);
+      },
+      unlockAccount: ['getGasPrice', 'getEstimateGas', function(data, callback) {
+        globals['eth-node'].eth.personal.unlockAccount(globals['eth-node'].defaultAccount.address, globals['eth-node'].defaultAccount.password, 1000, callback);
+      }]
+    }, (err, results) => {
+      if(err) {
+        console.error(err);
+        cb(err);
+        return;
+      }
+      //deploy
+      deployingContract.send({
+        from: globals['eth-node'].defaultAccount.address,
+        gas: results['getEstimateGas'],
+        gasPrice: results['getGasPrice']
+      }, cb)
+        .once('receipt', receipt => {
+          console.log('deploy contract receipt: ' + receipt.transactionHash);
+        })
+    })
+  }
 }
