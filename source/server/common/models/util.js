8b03d4b95f7ed5955526f6d42bb24abc5e8f8d16
CS Huynh Hoang <huynhphan@clearsystems.asia>
2017-11-24 22:01:45 +0700

    add async and underscore package, checkBalance api

--- source/server/common/models/util.js
+++ source/server/common/models/util.js
@@ -1,3 +1,7 @@
+const _ = require('underscore');
+const async = require('async');
+const globals = require('../../server/boot/globals');
+
 module.exports = function(Util) {
   Util.disableRemoteMethodByName('create');
   Util.disableRemoteMethodByName('count');
@@ -21,4 +25,23 @@ module.exports = function(Util) {
   Util.disableRemoteMethodByName('findById');
   Util.disableRemoteMethodByName('replaceById');
   Util.disableRemoteMethodByName('exists');
+
+  Util.remoteMethod('checkBalance', {
+    accepts: {arg: 'address', type: 'string', required: true},
+    http: {
+      verb: 'get'
+    },
+    returns: {
+      root: true, type: 'string'
+    }
+  })
+  Util.checkBalance = function(address, cb) {
+    if (!globals['eth-node'].web3.utils.isAddress(address)) {
+      let err = new Error('Address is not valid');
+      err.statusCode = 400;
+      cb(err);
+      return;
+    }
+    globals['eth-node'].eth.getBalance(address, cb);
+  }
 }
