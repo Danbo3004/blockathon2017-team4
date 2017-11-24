c405409ec6debe90fecb34923944bc1c0eade38d
CS Huynh Hoang <huynhphan@clearsystems.asia>
2017-11-25 01:22:50 +0700

    create sign transaction api

--- source/server/common/models/util.js
+++ source/server/common/models/util.js
@@ -1,6 +1,7 @@
 const _ = require('underscore');
 const async = require('async');
 const globals = require('../../server/boot/globals');
+const EthereumTx = require('ethereumjs-tx');
 
 module.exports = function(Util) {
   Util.disableRemoteMethodByName('create');
@@ -135,5 +136,76 @@ module.exports = function(Util) {
       return;
     }
     contract.methods[methodName].apply(contract.methods[methodName], args).call({}, cb);
+  }
+
+  Util.remoteMethod('transferEth', {
+    accepts: [
+      {arg: 'to', type: 'address', required: true},
+      {arg: 'wei', type: 'string', required: true},
+    ],
+    http: {
+      verb: 'post'
+    },
+    returns: {
+      arg: 'transactionHash', type: 'string'
+    }
+  })
+  Util.transferEth = function(to, wei, cb) {
+    if (!globals['eth-node'].web3.utils.isAddress(to)) {
+      let err = new Error('Receiver address is not valid');
+      err.statusCode = 400;
+      cb(err);
+      return;
+    }
+  }
+
+  Util.remoteMethod('signTransaction', {
+    accepts: [
+      {arg: 'privateKey', type: 'string', required: true},
+      {arg: 'to', type: 'string', required: true},
+      {arg: 'value', type: 'string', required: true},
+      {arg: 'data', type: 'string'}
+    ],
+    http: {
+      verb: 'get'
+    },
+    returns: {
+      arg: 'signedData', type: 'string'
+    }
+  })
+  Util.signTransaction = function(privateKey, to, value, data, cb) {
+    if (!globals['eth-node'].web3.utils.isAddress(to)) {
+      let err = new Error('Address is not valid');
+      err.statusCode = 400;
+      cb(err);
+      return;
+    }
+    async.auto({
+      getNonce: function(callback) {
+        const account = globals['eth-node'].eth.accounts.privateKeyToAccount(privateKey);
+        globals['eth-node'].eth.getTransactionCount(account.address, callback);
+      },
+      getGasPrice: function(callback) {
+        globals['eth-node'].eth.getGasPrice(callback);
+      }
+    }, (err, results) => {
+      if (err) {
+        cb(err);
+        return;
+      }
+      privateKey = Buffer.from(privateKey.length === 66 ? privateKey.slice(2) : privateKey, 'hex');
+      const txData = {
+        nonce: globals['eth-node'].web3.utils.toHex(results['getNonce']),
+        gasPrice: globals['eth-node'].web3.utils.toHex(results['getGasPrice']),
+        value: globals['eth-node'].web3.utils.toHex(value),
+        to: to,
+        data: data
+      };
+      const tempTx = new EthereumTx(txData);
+      txData.gas = globals['eth-node'].web3.utils.toHex(tempTx.getBaseFee().toString());
+      const tx = new EthereumTx(txData);
+      tx.sign(privateKey);
+      cb(null, '0x' + tx.serialize().toString('hex'));
+    })
   }
 }
