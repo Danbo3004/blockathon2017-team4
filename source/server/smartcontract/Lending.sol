e1b5d0d93b363c00bb424a3a738bda58e6541c25
CS Linh Trinh <linhtrinh@clearsystems.asia>
2017-11-25 03:49:20 +0700

    Update lending smart contract

--- source/server/smartcontract/Lending.sol
+++ source/server/smartcontract/Lending.sol
@@ -9,28 +9,49 @@ contract Lending {
     
     Token tk ;
 	address tokenAddress;
+	mapping(address => Loan) loans;
+	
     /* Constructor */
     function Lending(address token) {
         tk= Token(token);
+    }
+    
+    struct Loan{
+        uint value;
+        uint tu;
+        uint mau;
+        uint blockNumberExpired;
+        uint blockNumberTime;
+        bool isPaid;
     }
 
     function balanceOf() constant returns(uint256){
         return tk.balanceOf(this);
     }
 
-    event PickLending(address index from, uint256 value ,uint tu, uint mau,address index to);
-    
+    event PickLending(address indexed  from, uint value ,uint tu, uint mau,address indexed  to,uint blockNumber);
+	event PayDebt(address indexed  from, uint value ,address indexed  to,uint blockNumber);
+    event NewLoan(address indexed  from, uint value ,uint tu, uint mau,uint blockNumber,uint blockNumbeExpiredr,uint blockNumberTime);
 	//Lender pick %
-    function pickLending(address _from, uint256 _value ,uint tu, uint mau,address _to)  {
+    function pickLending(address _from, uint _value ,uint _tu, uint _mau,address _to)  {
 	    require(msg.sender == tokenAddress);
-		PickLending( _from,  _value , tu,  mau);        
+		PickLending( _from,  _value , _tu,  _mau,_to,block.number);        
 	}
     
 	
-	function transferPayDebt(address _from, uint256 _value){
+	function transferPayDebt(address _from, uint _value){
 		require(msg.sender == tokenAddress);
-
+		address lender;
+		PayDebt( _from,  _value , lender,block.number);
 	}
    
+    function createLoan(address _from,uint _value,uint _tu, uint _mau,uint _blockNumberExpires,uint _blockNumberTime){
+        Loan memory loan ;
+        require(loans[_from].value == 0 ||(loans[_from].value>0 && loans[_from].isPaid == true));
+         loan = Loan({value:_value,tu:_tu,mau:_mau,blockNumberExpired:_blockNumberExpires,blockNumberTime:_blockNumberTime,isPaid:false});
+        loans[_from]= loan;
+        NewLoan( _from, _value, _tu,  _mau,block.number, _blockNumberExpires, _blockNumberTime);
+    }
+    
     
 }
