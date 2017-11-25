9ef6260fed40afd337b16e92bd9f49c0a939c8b2
Tony Doan <tonydoan@vpsolution.co>
2017-11-26 00:44:35 +0700

    update borrower

--- source/client/src/app/views/account/borrower.component.ts
+++ source/client/src/app/views/account/borrower.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CreditService} from '../../services/credit.service';
+import {AuthenticationService} from '../../services/authentication';
 
 @Component({
   templateUrl: './borrower.component.html'
 })
-export class BorrowerComponent {
+export class BorrowerComponent implements OnInit {
+  listBorrowers: object[];
 
-  constructor() { }
+  constructor(private creditService: CreditService, private authenticationService: AuthenticationService) {
+  }
 
+  ngOnInit(): void {
+    const currentUser = this.authenticationService.getUser();
+    console.log(currentUser);
+
+    this.creditService.getCredits({
+      where: {
+        status: 'lending',
+        lenderId: currentUser.id
+      }
+    }, data => {
+      this.listBorrowers = data;
+    }, function (err) {
+      console.log(err);
+    })
+  }
 }
