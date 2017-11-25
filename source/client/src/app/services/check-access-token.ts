acf36516080ab54b8c3ced83ad204b72ddd93bf3
CS Huynh Hoang <huynhphan@clearsystems.asia>
2017-11-25 23:01:06 +0700

    fix storing access_token

--- source/client/src/app/services/check-access-token.ts
+++ source/client/src/app/services/check-access-token.ts
@@ -10,9 +10,8 @@ export class CheckAccessTokenService {
   subscription: Subscription;
   constructor(private http: HttpClient, private globalsService: GlobalsService, private router: Router) {
     this.baseUrl = globalsService.getBaseUrl() + 'users';
-    console.log('router: ' + router);
     if (sessionStorage.getItem('access_token')) {
-      this.subscription = this.http.get<any>(`${this.baseUrl}/count?access_token=${sessionStorage.getItem('access_token')}`)
+      this.subscription = this.http.get<any>(`${this.baseUrl}/count`)
         .subscribe(count => {
           this.router.navigateByUrl('account/wallet');
         }, err => {
