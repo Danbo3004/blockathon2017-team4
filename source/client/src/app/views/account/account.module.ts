75cb0d7e12e777162c16954053da505618edc09f
Tony Doan <tonydoan@vpsolution.co>
2017-11-26 06:36:02 +0700

    merge branch huynhphan

--- source/client/src/app/views/account/account.module.ts
+++ source/client/src/app/views/account/account.module.ts
@@ -6,10 +6,10 @@ import {BorrowerComponent} from './borrower.component';
 import {HistoryComponent} from './history.component';
 import {CommonModule} from "@angular/common";
 import {FormsModule} from '@angular/forms';
+import {AppPopupComponent} from "../../components/app-popup/app-popup.component";
 
 @NgModule({
   imports: [AccountRoutingModule, CommonModule, FormsModule],
-    FormsModule],
   declarations: [
     WalletComponent,
     OpportunitiesComponent,
