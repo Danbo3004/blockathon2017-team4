30c1f8aa250ae072b4fa50d3ef50840b596ef6ac
CS Huynh Hoang <huynhphan@clearsystems.asia>
2017-11-26 06:24:17 +0700

    create new bid, emit socket event

--- source/client/src/app/services/socket.service.ts
+++ source/client/src/app/services/socket.service.ts
@@ -4,13 +4,17 @@ import {OnDestroy} from '@angular/core';
 import * as io from 'socket.io-client/dist/socket.io';
 
 export class SocketService implements OnDestroy {
-  private url = 'https://lalalendapi.clearsystems.asia';
+  // private url = 'https://lalalendapi.clearsystems.asia';
+  private url = 'http://localhost:3000';
   private socket;
   constructor() {
     this.socket = io(this.url);
     this.socket.on('message', (data) => {
       console.log(data);
     });
+    this.socket.on('newBid', data => {
+      console.log(data);
+    })
   }
   ngOnDestroy(): void {
     this.socket.disconnect();
--- source/server/server/socket.js
+++ source/server/server/socket.js
@@ -1,15 +1,20 @@
 let socket = {};
 var app = require('./server');
+let io = {};
 
 module.exports = {
   init: (server) => {
-    const io = require('socket.io')(server);
+    io = require('socket.io')(server);
     io.on('connection', function (client) {
+      console.log('new connection');
     });
   },
   emitClient: (address, data) => {
     if (socket.emit) {
       socket.emit(address, data);
     }
+  },
+  broadcastMessage: (channel, message) => {
+    io.emit(channel, message);
   }
 }
